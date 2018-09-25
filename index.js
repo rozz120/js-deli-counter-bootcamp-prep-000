7c000f66747b200f3c1418972a24799595137be5
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-09-25 13:15:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ function takeANumber(lineOfPeople, name)
 {
   lineOfPeople++;
   return ("hi " + name ", you are number " + lineOfPeople + " in line");
+  "Welcome, Ada. You are number 1 in line."
 }
