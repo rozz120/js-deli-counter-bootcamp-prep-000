e3ecd64b9817e3429d010638a134ec7897531b96
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-09-25 13:28:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function takeANumber(number, name)
   var numberNumber = number.length;
   numberNumber++;
   var whatToReturn = ("Welcome, "  + name +  ". You are number "  + numberNumber +  " in line.");
-  //return whatToReturn
+  return whatToReturn
   
 }
+
+function 
